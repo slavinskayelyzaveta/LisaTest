2dd5e59532ead5e4e46447df2943fdc7848698b3
slavinskayelyzaveta <58368202+slavinskayelyzaveta@users.noreply.github.com>
2020-07-20 19:49:53 -0400

    any name

---
+++ src/TestsL/TestsByLisa.java
@@ -0,0 +1,12 @@
+package TestsL;
+
+public class TestsByLisa {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Love and respect yourself");
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module LisaTest {
+}
