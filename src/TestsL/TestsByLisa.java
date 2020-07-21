752e039e14bd03a246db2ab54b8d0be002712a57
slavinskayelyzaveta <58368202+slavinskayelyzaveta@users.noreply.github.com>
2020-07-20 20:04:48 -0400

    anything f

--- src/TestsL/TestsByLisa.java
+++ src/TestsL/TestsByLisa.java
@@ -5,7 +5,7 @@ public class TestsByLisa {
 	public static void main(String[] args) {
 		
 		System.out.println("Love and respect yourself");
-		
+		System.out.println("Surround yourself with smart people");
 
 	}
 
